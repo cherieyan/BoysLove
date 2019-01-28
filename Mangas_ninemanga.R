7c84f8e0db4548617bb79d7b4e85a129bf76520f
cherieyan <cherie.yanez@gmail.com>
2019-01-28 13:44:37 -0300

    Actualización data: gráficos y tablas

--- Mangas_ninemanga.R
+++ Mangas_ninemanga.R
@@ -1,8 +1,11 @@
+install.packages("ggplot2")
+
 ###  NINEMANGA ####
 #Extraer informacion de libreria rvest
 library('rvest')
+library(ggplot2)
 
-#Estraer links de las primeras 100 pasginas de ninemanga
+#Extraer links de las primeras 100 pasginas de ninemanga
 resultMangas <-list()
 for(i in 1:100){
   print(paste("http://es.ninemanga.com/category/index_",i,".html", sep= ""))
@@ -24,6 +27,7 @@ for(i in 1:100){
   Linkmangas2<-html_text(Contenido2)
   titulo2<-c(titulo2, Linkmangas2)
 }
+
 #Imprimir titulos de los mangas
 print(titulo2)
 
@@ -42,13 +46,60 @@ print(views)
 
 #Extraer generos de cada manga 
 Generos <-list()
-for(i in 1:5){
-  print(paste("Link ->",resultMangas[[i]],sep=""))
+for(i in 1:length(resultMangas)){
+  print(paste("Link ->es",resultMangas[[i]],sep=""))
   Descargas2<-read_html(resultMangas[[i]])
-  ContGenero<-html_nodes(Descargas2,"message.genre")
+  ContGenero<-html_nodes(Descargas2,"[itemprop=genre]")
   genero2<-html_text(ContGenero)
   Generos<-c(Generos, genero2)
 }
 
+#Imprimir generos
 print(Generos)
 
+#Cambiar n por ""
+Generos <- gsub("\n","", Generos)
+############## creando data frame de genero############
+unlistTitulosgenero <- unlist(Generos)
+
+tablagenero <- table(unlistTitulosgenero)
+
+titulogeneros <- as.data.frame(tablagenero)
+
+############# creando data frame nombres de animes ##############
+unlistTitulostitulos <- unlist(titulo2)
+
+tablatitulos<- table(unlistTitulostitulos)
+
+titulosname <- as.data.frame(tablatitulos)
+
+############# creando data frame views ###########
+unlistTitulosviews <- unlist(views)
+unlistTitulosviews <- gsub(",","",unlistTitulosviews)
+unlistTitulosviews <- gsub("views","",unlistTitulosviews)
+
+tablaviews<- table(as.numeric(unlistTitulosviews))
+
+titulosviews <- as.data.frame(tablaviews)
+
+#######uniendo tablas #############
+datos=cbind.data.frame(tablagenero,tablatitulos[1:1099],tablaviews[1:1099])
+
+# Generando una data para views 
+DATOS <- data.frame(LINKS = Linksmangas, TITULO = unlistTitulostitulos, VISITAS = as.numeric(unlistTitulosviews))
+
+write.csv(DATOS, file="TablaMangas.csv")
+write.csv(datos, file="TablitaMangas.csv")
+
+
+grafico_view <- ggplot(DATOS, aes(TITULO, VISITAS)) +
+  geom_bar(stat = "identity",fill = rgb(0.2, 0.2, 1, 0.3), color = "blue") +
+  coord_flip() + 
+  theme_minimal()
+
+#no salio
+
+grafico_genero <- ggplot(datos, aes(unlistTitulosgenero, Var1)) +
+  geom_bar(stat = "identity",fill = rgb(0.2, 0.2, 1, 0.3), color = "blue") +
+  coord_flip() + 
+  theme_minimal()
