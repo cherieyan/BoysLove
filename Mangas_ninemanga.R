637cb5faa725099ddc6c347f300ce4634af6138d
cherieyan <cherie.yanez@gmail.com>
2019-01-28 23:50:56 -0300

    Actualización

--- Mangas_ninemanga.R
+++ Mangas_ninemanga.R
@@ -1,11 +1,7 @@
-install.packages("ggplot2")
-
-###  NINEMANGA ####
-#Extraer informacion de libreria rvest
+### _NINEMANGA_ ###
 library('rvest')
-library(ggplot2)
 
-#Extraer links de las primeras 100 pasginas de ninemanga
+#Extraccion de los mangas de las primeras 100 paginas de resultados
 resultMangas <-list()
 for(i in 1:100){
   print(paste("http://es.ninemanga.com/category/index_",i,".html", sep= ""))
@@ -15,23 +11,22 @@ for(i in 1:100){
   resultMangas<-c(resultMangas, Linksmangas)
 }
 
-#imprimir los links 
 print(resultMangas)
 
-#Extraer titulos de los mangas
-titulo2 <-list()
+
+
+#Extraccion de los titulos
+titulos <-list()
 for(i in 1:100){
   print(paste("http://es.ninemanga.com/category/index_",i,".html", sep= ""))
   Leerlinks<-read_html(paste("http://es.ninemanga.com/category/index_",i,".html", sep= ""))
   Contenido2<-html_nodes(Leerlinks, '.bookname')
   Linkmangas2<-html_text(Contenido2)
-  titulo2<-c(titulo2, Linkmangas2)
+  titulos<-c(titulos, Linkmangas2)
 }
 
-#Imprimir titulos de los mangas
-print(titulo2)
 
-#Extraer numero de vistas de cada manga
+#Extraccion de las visitas
 views <-list()
 for(i in 1:100){
   print(paste("http://es.ninemanga.com/category/index_",i,".html", sep= ""))
@@ -41,55 +36,76 @@ for(i in 1:100){
   views<-c(views, Linkmangas3)
 }
 
-#Imprimir numero de visitas de cada manga
-print(views)
-
-#Extraer generos de cada manga 
+#Extraccion de los generos
 Generos <-list()
 for(i in 1:length(resultMangas)){
-  print(paste("Link ->es",resultMangas[[i]],sep=""))
+  print(paste("Link ->",resultMangas[[i]],sep=""))
   Descargas2<-read_html(resultMangas[[i]])
   ContGenero<-html_nodes(Descargas2,"[itemprop=genre]")
   genero2<-html_text(ContGenero)
   Generos<-c(Generos, genero2)
 }
 
-#Imprimir generos
-print(Generos)
 
-#Cambiar n por ""
-Generos <- gsub("\n","", Generos)
-############## creando data frame de genero############
-unlistTitulosgenero <- unlist(Generos)
 
-tablagenero <- table(unlistTitulosgenero)
-
-titulogeneros <- as.data.frame(tablagenero)
-
-############# creando data frame nombres de animes ##############
-unlistTitulostitulos <- unlist(titulo2)
-
+#Data frame de titulos
+unlistTitulostitulos <- unlist(titulos)
 tablatitulos<- table(unlistTitulostitulos)
-
 titulosname <- as.data.frame(tablatitulos)
 
-############# creando data frame views ###########
+
+#Data frame de views
 unlistTitulosviews <- unlist(views)
 unlistTitulosviews <- gsub(",","",unlistTitulosviews)
 unlistTitulosviews <- gsub("views","",unlistTitulosviews)
 
 tablaviews<- table(as.numeric(unlistTitulosviews))
-
 titulosviews <- as.data.frame(tablaviews)
 
-#######uniendo tablas #############
-datos=cbind.data.frame(tablagenero,tablatitulos[1:1099],tablaviews[1:1099])
 
-# Generando una data para views 
-DATOS <- data.frame(LINKS = Linksmangas, TITULO = unlistTitulostitulos, VISITAS = as.numeric(unlistTitulosviews))
+# Generando data de titulos y views
+DATOS <- data.frame(TITULO = unlistTitulostitulos, VISITAS = as.numeric(unlistTitulosviews))
 
-write.csv(DATOS, file="TablaMangas.csv")
-write.csv(datos, file="TablitaMangas.csv")
+
+#Data frame de generos
+unlistTitulosgenero <- unlist(Generos)
+unlistTitulosgenero <- gsub("Género","",unlistTitulosgenero)
+unlistTitulosgenero <- gsub("[()]","",unlistTitulosgenero)
+unlistTitulosgenero <- gsub("[:]","",unlistTitulosgenero)
+unlistTitulosgenero <- gsub("\n","-",unlistTitulosgenero)
+
+
+# Generando data de titulos views y genero
+DATOS2 <- data.frame(TITULO = unlistTitulostitulos[1:2458], VISITAS = as.numeric(unlistTitulosviews)[1:2458], GENERO = unlistTitulosgenero)
+write.csv(DATOS2,file="Tabla1.csv")
+
+
+#Separando por espacio los generos y pasando a minuscula
+unlistgeneros2 <- unlist(Generos)
+unlistgeneros2 <- gsub("Género","",unlistgeneros2)
+unlistgeneros2 <- gsub("[()]","",unlistgeneros2)
+unlistgeneros2 <- gsub("[:]","",unlistgeneros2)
+unlistgeneros2 <- gsub("\n","-",unlistgeneros2)
+
+unlistgeneros2 <- strsplit(unlistgeneros2,"-")
+unlistgeneros2 <- tolower(unlistgeneros2)
+
+
+print(unlistgeneros2)
+
+#unificar generos
+totalgeneros <- ""
+for(i in 1:length(unlistgeneros2)){
+  totalgeneros<-paste(totalgeneros," ",unlistgeneros2[[i]])
+}
+
+print(totalgeneros)
+
+#Tabla para extraer generos
+contgenre <- unlist(totalgeneros)
+tablagenre <- table(contgenre)
+
+dfGenero <- as.data.frame(tablagenre)
 
 
 grafico_view <- ggplot(DATOS, aes(TITULO, VISITAS)) +
@@ -97,8 +113,8 @@ grafico_view <- ggplot(DATOS, aes(TITULO, VISITAS)) +
   coord_flip() + 
   theme_minimal()
 
-#no salio
 
+#no salio
 grafico_genero <- ggplot(datos, aes(unlistTitulosgenero, Var1)) +
   geom_bar(stat = "identity",fill = rgb(0.2, 0.2, 1, 0.3), color = "blue") +
   coord_flip() + 
